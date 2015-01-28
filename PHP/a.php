2fcdc41a45e1152bc66eb3c1591dc15ce487e55d
rockman888 <huuvi168@gmail.com>
2015-01-28 17:03:56 +0700

    Update folder

---
+++ PHP/a.php
@@ -0,0 +1,10 @@
+<?php
+	// khi goi duong dan nay
+	// http://localhost/a.php?name=abc&&email=vilh@vng.com.vn
+	
+	// thi su dung $_GET
+	echo $_GET['name']. "</br>";
+	echo $_GET['email'];
+	
+?>
+
